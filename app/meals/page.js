f75860048a09d287ef6f8a32a3736a3193ca7844
uday <web.uday@spaarksweb.com>
2024-06-13 18:15:38 +0530

    loading

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,30 +1,39 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Classes from './page.module.css'
 import Link from 'next/link'
 import MEalsGrid from '@/component/meals/meals-grid'
 import { getMeals } from '@/lib/meals'
 
-export default async function MealsPage(){
 
- const  meals = await getMeals();
+async function Meals() {
+  const meals = await getMeals()
+  return <MEalsGrid meals={meals} />
+}
+
+
+export default async function MealsPage() {
+
+  const meals = await getMeals();
   return (
-  <>
-  <header className={Classes.header}>
-   <h1>Delicious meals,created {''} 
-    <span className={Classes.highlight}>by You </span>
-   </h1>
-   <p>Choose your favorite recipe and cook it yourself.it is easy and fun </p>
-  <p className={Classes.cta}>
-    
-    <Link href='/meals/share'>share your favorite recepie</Link>
-  </p>
-  
-  </header>
-  <main className={Classes.main}>
- <MEalsGrid meals={meals}/>
-  </main>
+    <>
+      <header className={Classes.header}>
+        <h1>Delicious meals,created {''}
+          <span className={Classes.highlight}>by You </span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.it is easy and fun </p>
+        <p className={Classes.cta}>
 
-  </>
+          <Link href='/meals/share'>share your favorite recepie</Link>
+        </p>
+
+      </header>
+      <main className={Classes.main}>
+        <Suspense fallback={  <p className={Classes.loading}>Loading...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+
+    </>
   )
 }
 
--- component/main-header/main-header.js
+++ component/main-header/main-header.js
@@ -22,7 +22,7 @@ const MainHeader = () => {
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href='/meals' className={path.startsWith('/meals')? classes.active:undefined}>Brouse Meals </Link>
+                        <Link href='/meals' className={path.startsWith('/meals')? classes.active:undefined}>BrouseMeals </Link>
                     </li>
                     <li>
                         <Link href='/community' className={path.startsWith('/community')?classes.active:undefined}>Community</Link>
