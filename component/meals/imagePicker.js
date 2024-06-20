776a7423376d762141a36c30c45abfeed312b01b
uday <web.uday@spaarksweb.com>
2024-06-20 15:22:55 +0530

    image uploaded feature added

---
+++ component/meals/imagePicker.js
@@ -0,0 +1,57 @@
+'use client'
+
+import { useRef, useState } from 'react'
+import classes from './imagePicker.module.css'
+import Image from 'next/image'
+
+
+const ImagePicker = ({ label, name }) => {
+    const [pickedImage, setPickedImage] = useState()
+    const imageInput = useRef()
+
+    function handleImageChange(event) {
+        const file = event.target.files[0]
+        if (!file) {
+            return
+        }
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result)
+        }
+        fileReader.readAsDataURL(file)
+    }
+
+    function pickClick() {
+        imageInput.current.click()
+
+    }
+
+
+    return (
+        <div className={classes.picker}>
+
+            <label htmlFor={name}>{label}</label>
+            <div className={classes.controls}>
+                <div>
+                    {!pickedImage &&
+                        <p>No Image picked</p>
+
+                    }
+                    {pickedImage && <Image src={pickedImage} alt='image selected user' width={200} height={200} />
+                    }
+                </div>
+                <input className={classes.input}
+                    ref={imageInput}
+                    type='file' id={name}
+                    accept='image/png, image/jpeg' name {...name}
+                    onChange={handleImageChange}
+
+                />
+                <button className={classes.button} type='button' onClick={pickClick}>pick an image</button>
+            </div>
+        </div>
+    )
+}
+
+export default ImagePicker
